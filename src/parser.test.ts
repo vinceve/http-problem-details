ce2db58272159396815702d85099bb743fbe3d5d
Vince Verberckt <vince@verberckt.com>
2019-12-22 17:36:26 +0100

    Parse problem

--- src/parser.test.ts
+++ src/parser.test.ts
@@ -1,26 +1,41 @@
-import { sum } from './math';
 import { Parser } from './parser';
 
 describe('parser', () => {
   describe('read', () => {
     it('throws an error when no type is present', () => {
-      expect(() => new Parser().read({}, 400)).toThrowError('Data does not contain a problem definition.');
+      expect(() => new Parser().read({})).toThrowError('Data does not contain a problem definition.');
     });
 
-    it('sums 2 numbers', () => {
-      expect(sum(1, 1)).toBe(2);
+    it('parses a type from an incoming object', () => {
+      expect(new Parser().read({ type: 'https://verberckt.io/problem-type' })).toEqual({ type: 'https://verberckt.io/problem-type' });
     });
 
-    it('sums n numbers', () => {
-      expect(sum(1, 2, 3, 4)).toBe(10);
+    it('parses a type from an incoming string', () => {
+      expect(new Parser().read(`{ "type": "https://verberckt.io/problem-type" }`)).toEqual({ type: 'https://verberckt.io/problem-type' });
     });
 
-    it('returns a number if it is provided by itself', () => {
-      expect(sum(10)).toBe(10);
+    it('parses a optional title from an incoming string', () => {
+      expect(new Parser().read(`{ "type": "https://verberckt.io/problem-type", "title": "a test" }`)).toEqual({ type: 'https://verberckt.io/problem-type', title: 'a test' });
     });
 
-    it('returns 0 if no numbers are provided', () => {
-      expect(sum()).toBe(0);
+    it('parses a optional detail from an incoming object', () => {
+      expect(new Parser().read({ type: 'https://verberckt.io/problem-type', detail: 'test' })).toEqual({ type: 'https://verberckt.io/problem-type', detail: 'test' });
+    });
+
+    it('parses a optional instance from an incoming object', () => {
+      expect(new Parser().read({ type: 'https://verberckt.io/problem-type', instance: 'https://verberckt.io/error/1234' })).toEqual({ type: 'https://verberckt.io/problem-type', instance: 'https://verberckt.io/error/1234' });
+    });
+
+    it('parses a status code from an incoming object', () => {
+      expect(new Parser().read({ type: 'https://verberckt.io/problem-type', status: 400 })).toEqual({ type: 'https://verberckt.io/problem-type', status: 400 });
+    });
+
+    it('parses a status code from the response itself', () => {
+      expect(new Parser().read({ type: 'https://verberckt.io/problem-type' }, 400)).toEqual({ type: 'https://verberckt.io/problem-type', status: 400 });
+    });
+
+    it('takes the object status over the response status', () => {
+      expect(new Parser().read({ type: 'https://verberckt.io/problem-type', status: 422 }, 400)).toEqual({ type: 'https://verberckt.io/problem-type', status: 422 });
     });
   });
 });
--- src/parser.ts
+++ src/parser.ts
@@ -1,11 +1,26 @@
 import { Problem } from './types/problem';
 
 export class Parser {
+  read(incomingData: object | string, code?: number): Problem {
+    const data = typeof incomingData === 'string' ? JSON.parse(incomingData) : incomingData;
 
-  read(data: any, status: number): Problem {
     if (!data.type) {
       throw new Error("Data does not contain a problem definition.");
     }
-  }
 
+    const { type, title, detail, instance, status } = data;
+
+    let outgoingMessage: Problem = {
+      type,
+      title,
+      detail,
+      instance
+    };
+
+    if (status || code) {
+      outgoingMessage = { ...outgoingMessage, status: status ? status : code };
+    }
+
+    return outgoingMessage;
+  }
 }
